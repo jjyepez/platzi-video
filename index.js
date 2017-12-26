bc957b6785e64b2c4ffc26fdd21600b15dadad20
jjyepez <jjyepez@gmail.com>
2017-12-26 03:52:53 -0400

    Clase 9

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import React 	from 'react'
 import ReactDOM from 'react-dom'
+import Media from './src/playlisyt/componentes/media.js'
 
 const $app = document.getElementById( 'app' )
 
 // -- ReactDOM.render(que voy a renderizar, donde lo hare )
 // 	  el elemento que se va a renderizar podría der o un bloque de codigo JSX o un componente de React
-const html = <h1>Jai pipol!</h1>
-ReactDOM.render( html, $app )
+//const html = <h1>Jai pipol!</h1>
+ReactDOM.render( <Media />, $app )
---
+++ src/playlisyt/componentes/media.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Media extends Component {
+	render() {
+		return (
+			<div>
+				<div>
+					<img
+						src=""
+						alt=""
+						width={260}
+						height={160}
+					/>
+					<h3>¿Por qué aprender react?</h3>
+					<p>Leonidas Esteban</p>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Media;
