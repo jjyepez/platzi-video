f6625c9160747345664c188aadb98cf2f8d20092
jjyepez <jjyepez@gmail.com>
2017-12-28 05:35:22 -0400

    Avances en Video Player personalizado - Clase 29

---
+++ src/player/components/title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './title.css'
+
+const Title = props => (
+  <div className="Title">
+    <h2>{props.title}</h2>
+  </div>
+)
+
+export default Title
--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import VideoPlayerLayout from '../components/video-player-layout'
+import Title from '../components/title'
+import Video from '../containers/video'
 
 class VideoPlayerContainer extends Component {
   render(){
     return(
       <VideoPlayerLayout>
-        <video
-          controls
-          autoPlay
-          src = "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+        <Title
+          title = "Este es el título del video que se est[a reproduciendo]"
+        />
+        <Video
+          autoplay = {true}
+          src      = "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
         />
       </VideoPlayerLayout>
     )
