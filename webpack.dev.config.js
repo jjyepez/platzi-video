59b2b5e963f680902a9735f54d04bba9c7deb8e4
jjyepez <jjyepez@gmail.com>
2018-01-02 15:23:53 -0400

    Ajustes menores css

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -37,8 +37,8 @@ module.exports = {
           loader: 'url-loader',
           options: {
             limit: 1000000,
-            fallback: 'file-loader',
-            name: 'img/[name].[hash].[ext]',
+            fallback: 'file-loader'
+            // --- name: 'img/[name].[hash].[ext]',
           }
         }
       }
