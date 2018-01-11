af5139efaeed94dff4aa74e82c00737eda29d4a3
jjyepez <jjyepez@gmail.com>
2018-01-10 22:44:14 -0400

    Agregado un enhancer / middleware de terceros

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,16 +2,18 @@ import React 	  from 'react'
 import ReactDOM from 'react-dom'
 import Home		  from '../pages/containers/home'
 // --- import data 	from '../data/api.json'
-import extras	from '../data/api-extra.json'
+import extras		from '../data/api-extra.json'
 import '../css/style.css'
 
 // --- Redux
-import { Provider }    from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import reducer     		 from '../reducers/index'
-import data  from '../schemas/index.js'
-
+import { Provider }   from 'react-redux'
+import reducer     		from '../reducers/index'
+import data  					from '../schemas/index.js'
 import { Map as map } from 'immutable' // --- se le coloca un alias en minusculas para verlo como funcion no como clase
+
+// --- Middlewares
+import logger 				from 'redux-logger'
 
 // ----- versión pre-ES6
 // function logger({ dispatch, getState }){
@@ -27,19 +29,19 @@ import { Map as map } from 'immutable' // --- se le coloca un alias en minuscula
 // }
 
 // --- versión ES6
-const logger = ({ dispatch, getState }) => next => action => {
-	console.log( 'estado anterior:', getState().toJS() )
-	console.log( 'enviando acción:', action)
-	const rslt = next( action )
-	console.log( 'nuevo estado   :', getState().toJS() )
-	return rslt
-}
+// const logger = ({ dispatch, getState }) => next => action => {
+// 	console.log( 'estado anterior:', getState().toJS() )
+// 	console.log( 'enviando acción:', action)
+// 	const rslt = next( action )
+// 	console.log( 'nuevo estado   :', getState().toJS() )
+// 	return rslt
+// }
 
 const store = createStore(
 	reducer,
 	map(), // --- se va a definir el estado inicial de cada reducer en su propio archivo js
 	applyMiddleware( logger )
-	// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // --- requiere la extensión/plugin instalada en el navegador!
 )
 
 const $homeContainer = document.getElementById( 'home-container' )
